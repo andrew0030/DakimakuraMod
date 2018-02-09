7bf4d6d4f39647d2ecea9eec7b67e229e6c6d960
RiskyKen <RiskyKen@users.noreply.github.com>
2018-02-09 22:53:55 +0000

    Fixed item rendering size being too small.

--- src/main/java/moe/plushie/dakimakuramod/client/render/item/RenderItemDakimakura.java
+++ src/main/java/moe/plushie/dakimakuramod/client/render/item/RenderItemDakimakura.java
@@ -1,5 +1,7 @@
 package moe.plushie.dakimakuramod.client.render.item;
 
+import org.lwjgl.opengl.GL11;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import moe.plushie.dakimakuramod.client.model.ModelDakimakura;
@@ -30,6 +32,29 @@ public class RenderItemDakimakura implements IItemRenderer {
     @Override
     public void renderItem(ItemRenderType renderType, ItemStack itemStack, Object... data) {
         Daki daki = DakiNbtSerializer.deserialize(itemStack.getTagCompound());
+        GL11.glPushMatrix();
+        float scale = 1F / 16F;
+        if (renderType == ItemRenderType.INVENTORY) {
+            GL11.glTranslated(0, -3.5F * scale, 0);
+            GL11.glRotatef(180, 0, 1, 0);
+            GL11.glScalef(1.1F, 1.1F, 1.1F);
+        }
+        if (renderType == ItemRenderType.EQUIPPED_FIRST_PERSON) {
+            GL11.glRotatef(90, 0, 1, 0);
+            GL11.glTranslated(0, 12 * scale, 0);
+            GL11.glTranslated(-8 * scale, 4 * scale, -3 * scale);
+        }
+        if (renderType == ItemRenderType.EQUIPPED) {
+            
+            GL11.glScalef(3F, 3F, 3F);
+            GL11.glTranslated(3 * scale, 0, 4 * scale);
+            GL11.glRotatef(180, 0, 1, 0);
+        }
+        if (renderType == ItemRenderType.ENTITY) {
+            GL11.glTranslated(0, 24 * scale, 0);
+            GL11.glScalef(3F, 3F, 3F);
+        }
         modelDakimakura.render(daki);
+        GL11.glPopMatrix();
     }
 }
