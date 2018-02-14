62e067f18c996f45fd9b792ca404abe0b658c05f
RiskyKen <RiskyKen@users.noreply.github.com>
2018-02-14 17:29:01 +0000

    Got the daki design crafting working.

--- src/main/java/moe/plushie/dakimakuramod/common/crafting/CraftingManager.java
+++ src/main/java/moe/plushie/dakimakuramod/common/crafting/CraftingManager.java
@@ -4,12 +4,17 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import moe.plushie.dakimakuramod.common.block.ModBlocks;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.RecipeSorter;
+import net.minecraftforge.oredict.RecipeSorter.Category;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 public final class CraftingManager {
 
     public static void init() {
+        RecipeSorter.INSTANCE.register("dakimakuramod:shapeless", RecipeDaki.class, Category.SHAPELESS, "after:minecraft:shapeless");
+        GameRegistry.addRecipe(new RecipeDaki());
+        
         addShapedRecipe(new ItemStack(ModBlocks.blockDakimakura, 1, 0), new Object[] {
                 "ww",
                 "ww",
---
+++ src/main/java/moe/plushie/dakimakuramod/common/crafting/RecipeDaki.java
@@ -0,0 +1,120 @@
+package moe.plushie.dakimakuramod.common.crafting;
+
+import moe.plushie.dakimakuramod.DakimakuraMod;
+import moe.plushie.dakimakuramod.common.block.ModBlocks;
+import moe.plushie.dakimakuramod.common.dakimakura.Daki;
+import moe.plushie.dakimakuramod.common.dakimakura.serialize.DakiNbtSerializer;
+import moe.plushie.dakimakuramod.common.items.ModItems;
+import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+
+public class RecipeDaki implements IRecipe {
+
+    @Override
+    public boolean matches(InventoryCrafting inventoryCrafting, World world) {
+        ItemStack stackDaki = null;
+        ItemStack stackDesign = null;
+        
+        for (int slotId = 0; slotId < inventoryCrafting.getSizeInventory(); slotId++) {
+            ItemStack stack = inventoryCrafting.getStackInSlot(slotId);
+            if (stack != null) {
+                if (stack.getItem() == ModItems.dakiDesign) {
+                    if (stackDesign == null) {
+                        stackDesign = stack;
+                    } else {
+                        return false;
+                    }
+                } else if (stack.getItem() == Item.getItemFromBlock(ModBlocks.blockDakimakura)) {
+                    if (stackDaki == null) {
+                        stackDaki = stack;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+        
+        if (stackDesign == null) {
+            return false;
+        }
+        if (stackDaki == null) {
+            return false;
+        }
+        
+        Daki dakiDesign = DakiNbtSerializer.deserialize(stackDesign.getTagCompound());
+        if (dakiDesign == null) {
+            return false;
+        }
+        Daki dakiBlack = DakiNbtSerializer.deserialize(stackDaki.getTagCompound());
+        if (dakiBlack != null) {
+            return false;
+        }
+        
+        return true;
+    }
+
+    @Override
+    public ItemStack getCraftingResult(InventoryCrafting inventoryCrafting) {
+        ItemStack stackDaki = null;
+        ItemStack stackDesign = null;
+        
+        for (int slotId = 0; slotId < inventoryCrafting.getSizeInventory(); slotId++) {
+            ItemStack stack = inventoryCrafting.getStackInSlot(slotId);
+            if (stack != null) {
+                if (stack.getItem() == ModItems.dakiDesign) {
+                    if (stackDesign == null) {
+                        stackDesign = stack;
+                    } else {
+                        return null;
+                    }
+                } else if (stack.getItem() == Item.getItemFromBlock(ModBlocks.blockDakimakura)) {
+                    if (stackDaki == null) {
+                        stackDaki = stack;
+                    } else {
+                        return null;
+                    }
+                } else {
+                    return null;
+                }
+            }
+        }
+        
+        if (stackDesign == null) {
+            return null;
+        }
+        if (stackDaki == null) {
+            return null;
+        }
+        
+        Daki dakiDesign = DakiNbtSerializer.deserialize(stackDesign.getTagCompound());
+        if (dakiDesign == null) {
+            return null;
+        }
+        DakimakuraMod.getLogger().info(dakiDesign);
+        ItemStack result = stackDaki.copy();
+        
+        if (!result.hasTagCompound()) {
+            result.setTagCompound(new NBTTagCompound());
+        }
+        DakiNbtSerializer.serialize(dakiDesign, result.getTagCompound());
+        
+        return result;
+    }
+
+    @Override
+    public int getRecipeSize() {
+        return 2;
+    }
+
+    @Override
+    public ItemStack getRecipeOutput() {
+        return null;
+    }
+    
+}
