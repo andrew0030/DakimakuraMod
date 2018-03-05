b398806614395474936e7e8901dcc4c2c943cbf3
RiskyKen <RiskyKen@users.noreply.github.com>
2018-03-05 23:09:37 +0000

    Sort of fixed placement preview.

--- src/main/java/moe/plushie/dakimakuramod/client/handler/PlacementPreviewHandler.java
+++ src/main/java/moe/plushie/dakimakuramod/client/handler/PlacementPreviewHandler.java
@@ -1,7 +1,17 @@
 package moe.plushie.dakimakuramod.client.handler;
 
+import org.lwjgl.opengl.GL11;
+
 import moe.plushie.dakimakuramod.client.model.ModelDakimakura;
+import moe.plushie.dakimakuramod.common.block.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.DrawBlockHighlightEvent;
@@ -30,52 +40,52 @@ public class PlacementPreviewHandler {
         }
         float partialTicks = event.getPartialTicks();
         
-        /*
-        int x = target.blockX;
-        int y = target.blockY;
-        int z = target.blockZ;
-        int side = target.sideHit;
+        BlockPos pos = target.getBlockPos();
+        EnumFacing facing = target.sideHit;
         
         double xOff = entityPlayer.lastTickPosX + (entityPlayer.posX - entityPlayer.lastTickPosX) * partialTicks;
         double yOff = entityPlayer.lastTickPosY + (entityPlayer.posY - entityPlayer.lastTickPosY) * partialTicks;
         double zOff = entityPlayer.lastTickPosZ + (entityPlayer.posZ - entityPlayer.lastTickPosZ) * partialTicks;
         
-        if (event.currentItem != null && event.currentItem.getItem() == Item.getItemFromBlock(ModBlocks.blockDakimakura)) {
-            Block block = world.getBlock(x, y, z);
-            ForgeDirection sideDir = ForgeDirection.getOrientation(side);
+        if (entityPlayer.getHeldItemMainhand() != null && entityPlayer.getHeldItemMainhand().getItem() == Item.getItemFromBlock(ModBlocks.blockDakimakura)) {
+            IBlockState iblockstate = world.getBlockState(pos);
+            Block block = iblockstate.getBlock();
+            if (!block.isReplaceable(world, pos)) {
+                pos = pos.offset(facing);
+            }
             int rot = (MathHelper.floor_double((double)(entityPlayer.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3);
-            ForgeDirection[] rots = new ForgeDirection[] {ForgeDirection.NORTH, ForgeDirection.EAST, ForgeDirection.SOUTH, ForgeDirection.WEST};
-            ForgeDirection rotation = rots[rot].getOpposite();
+            EnumFacing[] rots = new EnumFacing[] {EnumFacing.NORTH, EnumFacing.EAST, EnumFacing.SOUTH, EnumFacing.WEST};
+            EnumFacing rotation = rots[rot].getOpposite();
+            
+            
             boolean standing = false;
-            if ((block == Blocks.snow_layer && (world.getBlockMetadata(x, y, y) & 7) < 1) | block == Blocks.tallgrass) {
-                sideDir = ForgeDirection.UP;
-            } else if (block != Blocks.vine && block != Blocks.tallgrass && block != Blocks.deadbush && !block.isReplaceable(world, x, y, y)) {
-                x += sideDir.offsetX;
-                y += sideDir.offsetY;
-                z += sideDir.offsetZ;
-            }
-            if (sideDir != ForgeDirection.UP & sideDir != ForgeDirection.DOWN) {
-                rotation = sideDir.getOpposite();
+            
+            
+            if (facing != EnumFacing.UP & facing != EnumFacing.DOWN) {
                 standing = true;
+                rotation = facing.getOpposite();
             }
-            if (sideDir == ForgeDirection.DOWN) {
-                y--;
+            if (facing == EnumFacing.DOWN) {
                 standing = true;
+                //rotation = EnumFacing.UP;
+                
+                pos = pos.offset(EnumFacing.DOWN);
             }
+            
             GL11.glPushMatrix();
-            GL11.glTranslated(-xOff + x, -yOff + y, -zOff + z);
+            GL11.glTranslated(-xOff + pos.getX(), -yOff + pos.getY(), -zOff + pos.getZ());
             float scale = 0.0625F;
             GL11.glTranslatef(8F * scale, 2F * scale, 8F * scale);
-            if (rotation == ForgeDirection.WEST) {
+            if (rotation == EnumFacing.WEST) {
                 GL11.glRotatef(-90, 0, 1, 0);
             }
-            if (rotation == ForgeDirection.NORTH) {
+            if (rotation == EnumFacing.NORTH) {
                 GL11.glRotatef(180, 0, 1, 0);
             }
-            if (rotation == ForgeDirection.EAST) {
+            if (rotation == EnumFacing.EAST) {
                 GL11.glRotatef(90, 0, 1, 0);
             }
-            if (block.isBed(world, x, y, z, entityPlayer) & side == 1) {
+            if (block.isBed(world.getBlockState(pos), world, pos, entityPlayer) & facing == EnumFacing.UP) {
                 GL11.glTranslatef(0F * scale, -7F * scale, 0);
             }
             
@@ -95,6 +105,8 @@ public class PlacementPreviewHandler {
             GL11.glDisable(GL11.GL_TEXTURE_2D);
             GL11.glDepthMask(false);
             GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_LINE);
+            GL11.glDisable(GL11.GL_LIGHTING);
+            GL11.glDisable(GL11.GL_NORMALIZE);
             modelDakimakura.render(null);
             GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_FILL);
             GL11.glDepthMask(true);
@@ -102,7 +114,7 @@ public class PlacementPreviewHandler {
             GL11.glDisable(GL11.GL_BLEND);
             GL11.glPopAttrib();
             GL11.glPopMatrix();
+            
         }
-        */
     }
 }
