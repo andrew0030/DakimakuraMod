5e87dc33ccf3fbad73e4bea9a16a375d76014734
RiskyKen <RiskyKen@users.noreply.github.com>
2018-03-06 08:42:57 +0000

    Updated model renderer to use the new model class.

--- src/main/java/moe/plushie/dakimakuramod/client/model/ModelDakimakura.java
+++ src/main/java/moe/plushie/dakimakuramod/client/model/ModelDakimakura.java
@@ -13,10 +13,9 @@ import moe.plushie.dakimakuramod.proxies.ClientProxy;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.GLAllocation;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.profiler.Profiler;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.model.AdvancedModelLoader;
-import net.minecraftforge.client.model.IModelCustom;
 
 @SideOnly(Side.CLIENT)
 public class ModelDakimakura extends ModelBase {
@@ -24,13 +23,13 @@ public class ModelDakimakura extends ModelBase {
     private static final ResourceLocation MODEL_LOCATION = new ResourceLocation(LibModInfo.ID, "models/bolster-new-uv.obj");
     private static final ResourceLocation TEXTURE_BLANK = new ResourceLocation(LibModInfo.ID, "textures/models/blank.png");
     
-    private final IModelCustom DAKIMAKURA_MODEL;
+    private final ObjModel DAKIMAKURA_MODEL;
     private final DakiTextureManagerClient dakiTextureManager;
     private final Profiler profiler;
     private int modelList = -1;
     
     public ModelDakimakura(DakiTextureManagerClient dakiTextureManager) {
-        DAKIMAKURA_MODEL = AdvancedModelLoader.loadModel(MODEL_LOCATION);
+        DAKIMAKURA_MODEL = ObjModel.loadModel(MODEL_LOCATION);
         this.dakiTextureManager = dakiTextureManager;
         profiler = Minecraft.getMinecraft().mcProfiler;
     }
@@ -40,7 +39,7 @@ public class ModelDakimakura extends ModelBase {
         if (daki == null) {
             Minecraft.getMinecraft().renderEngine.bindTexture(TEXTURE_BLANK);
         } else {
-            DakiTexture dakiTexture = ((ClientProxy)DakimakuraMod.getProxy()).getDakiTextureManager().getTextureForDaki(daki);
+            DakiTexture dakiTexture = ((ClientProxy) DakimakuraMod.getProxy()).getDakiTextureManager().getTextureForDaki(daki);
             if (dakiTexture.isLoaded()) {
                 GL11.glBindTexture(GL11.GL_TEXTURE_2D, dakiTexture.getGlTextureId());
             } else {
@@ -49,23 +48,65 @@ public class ModelDakimakura extends ModelBase {
         }
         profiler.endStartSection("model");
         GL11.glPushMatrix();
-        GL11.glPushAttrib(GL11.GL_POLYGON_BIT | GL11.GL_ENABLE_BIT);
-        GL11.glCullFace(GL11.GL_FRONT);
-        GL11.glEnable(GL11.GL_CULL_FACE);
+        GL11.glPushAttrib(GL11.GL_POLYGON_BIT | GL11.GL_ENABLE_BIT | GL11.GL_LIGHTING_BIT);
+        // Enable bit.
         GL11.glEnable(GL11.GL_NORMALIZE);
+        
+        // Polygon bit.
+        GL11.glCullFace(GL11.GL_BACK);
+        GL11.glEnable(GL11.GL_CULL_FACE);
+        
+        // Lighting bit.
+        GL11.glShadeModel(GL11.GL_SMOOTH);
+        
         GL11.glColor4f(1, 1, 1, 1);
-        GL11.glScalef(-1, 1, 1);
         GL11.glScalef(0.55F, 0.55F, 0.55F);
         GL11.glTranslatef(0, 0.35F, 0);
-        if (modelList == -1) {
+        GL11.glScalef(-1, -1, 1);
+        
+        if (modelList == -1) { 
             modelList = GLAllocation.generateDisplayLists(1);
             GL11.glNewList(modelList, GL11.GL_COMPILE);
-            DAKIMAKURA_MODEL.renderAll();
+            DAKIMAKURA_MODEL.render();
             GL11.glEndList();
         }
         GL11.glCallList(modelList);
+        
         GL11.glPopAttrib();
         GL11.glPopMatrix();
         profiler.endSection();
+    }
+    
+    private void drawDebugRender() {
+        Tessellator tess = Tessellator.instance;
+        tess.startDrawing(GL11.GL_QUADS);
+        // Front
+        
+        tess.addVertex(-0.5F, 1.5F, -0.2F);
+        tess.setTextureUV(0, 0);
+        
+        tess.addVertex(0.5F, 1.5F, -0.2F);
+        tess.setTextureUV(0.5F, 0);
+        
+        tess.addVertex(0.5F, -1.5F, -0.2F);
+        tess.setTextureUV(0.5F, 1);
+        
+        tess.addVertex(-0.5F, -1.5F, -0.2F);
+        tess.setTextureUV(0, 1);
+        
+        // Back
+        tess.addVertex(-0.5F, -1.5F, 0);
+        tess.setTextureUV(0.5F, 1);
+        
+        tess.addVertex(0.5F, -1.5F, 0);
+        tess.setTextureUV(1, 1);
+        
+        tess.addVertex(0.5F, 1.5F, 0);
+        tess.setTextureUV(1, 0);
+        
+        tess.addVertex(-0.5F, 1.5F, 0);
+        tess.setTextureUV(0.5F, 0);
+        
+        tess.draw();
     }
 }
