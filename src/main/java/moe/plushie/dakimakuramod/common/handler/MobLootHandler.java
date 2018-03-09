eee5fd2c38c4d846290f54e93bf6226b43976bd0
RiskyKen <RiskyKen@users.noreply.github.com>
2018-03-09 11:53:59 +0000

    Fixed mob drops.

--- src/main/java/moe/plushie/dakimakuramod/common/handler/MobLootHandler.java
+++ src/main/java/moe/plushie/dakimakuramod/common/handler/MobLootHandler.java
@@ -1,5 +1,12 @@
 package moe.plushie.dakimakuramod.common.handler;
 
+import moe.plushie.dakimakuramod.common.config.ConfigHandler;
+import moe.plushie.dakimakuramod.common.items.ModItems;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
@@ -13,23 +20,21 @@ public class MobLootHandler {
     
     @SubscribeEvent(priority=EventPriority.LOW)
     public void onLivingDropsEvent(LivingDropsEvent event) {
-        /*
-        if (event.entity instanceof EntityMob) {
-            EntityLivingBase entity = event.entityLiving;
+        if (event.getEntity() instanceof EntityMob) {
+            EntityLivingBase entity = event.getEntityLiving();
             World world = entity.worldObj;
-            if (world.getGameRules().getGameRuleBooleanValue("doMobLoot")) {
+            if (world.getGameRules().getBoolean("doMobLoot")) {
                 float dropChance = ConfigHandler.mobDropChance;
                 float lootingBonus = ConfigHandler.mobDropLootingBonus;
                 if (dropChance > 0F) {
-                    dropChance += lootingBonus * (float)event.lootingLevel;
+                    dropChance += lootingBonus * (float)event.getLootingLevel();
                     if (world.rand.nextFloat() * 100F <= dropChance) {
                         ItemStack itemStack = new ItemStack(ModItems.dakiDesign);
                         EntityItem entityItem = new EntityItem(world, entity.posX, entity.posY, entity.posZ, itemStack);
-                        event.drops.add(entityItem);
+                        event.getDrops().add(entityItem);
                     }
                 }
             }
         }
-        */
     }
 }
