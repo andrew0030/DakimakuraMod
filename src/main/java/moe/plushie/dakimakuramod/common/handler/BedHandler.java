3b9593473d23bc70a51bdd379fcde5b4cf5e4a40
RiskyKen <RiskyKen@users.noreply.github.com>
2018-03-10 10:59:45 +0000

    Sort of fixed dakis on beds.

--- src/main/java/moe/plushie/dakimakuramod/common/handler/BedHandler.java
+++ src/main/java/moe/plushie/dakimakuramod/common/handler/BedHandler.java
@@ -1,5 +1,16 @@
 package moe.plushie.dakimakuramod.common.handler;
 
+import java.util.ArrayList;
+
+import moe.plushie.dakimakuramod.DakimakuraMod;
+import moe.plushie.dakimakuramod.common.entities.EntityDakimakura;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -12,50 +23,49 @@ public class BedHandler {
     
     @SubscribeEvent
     public void onPlayerInteract(PlayerInteractEvent event) {
-        /*
-        if (event.action == Action.RIGHT_CLICK_AIR) {
+        if (event.getFace() == null) {
             return;
         }
-        World world = event.world;
-        EntityPlayer entityPlayer = event.entityPlayer;
-        int x = event.x;
-        int y = event.y;
-        int z = event.z;
-        Block block = world.getBlock(x, y, z);
+        World world = event.getWorld();
+        EntityPlayer entityPlayer = event.getEntityPlayer();
+        BlockPos blockPos = event.getPos();
+        IBlockState blockState = world.getBlockState(blockPos);
         if (world.isRemote) {
             return;
         }
-        if (!block.isBed(world, x, y, z, entityPlayer)) {
+        if (!blockState.getBlock().isBed(blockState, world, blockPos, entityPlayer)) {
             return;
         }
         if (!entityPlayer.isSneaking()) {
             return;
         }
-        AxisAlignedBB aabb = AxisAlignedBB.getBoundingBox(x - 1, y + 1, z - 1, x + 2, y + 2, z + 2);
+        DakimakuraMod.getLogger().info("Over block " + event.getHand());
+        AxisAlignedBB aabb = new AxisAlignedBB(blockPos.add(-1, 0, -1), blockPos.add(1, 2, 1));
         ArrayList entityList = (ArrayList) world.getEntitiesWithinAABB(EntityDakimakura.class, aabb);
         if (entityList.isEmpty()) {
             return;
         }
         boolean flipped = false;
+        
         for (int i = 0; i < entityList.size(); i++) {
             Entity entity = (Entity) entityList.get(i);
             if (entity instanceof EntityDakimakura) {
                 EntityDakimakura entityDaki = (EntityDakimakura) entity;
-                if (entityDaki.isDakiOverBlock(x, y, z)) {
-                    if (event.action == Action.RIGHT_CLICK_BLOCK) {
+                if (entityDaki.isDakiOverBlock(blockPos)) {
+                    if (event.getHand() == EnumHand.MAIN_HAND) {
                         entityDaki.flip();
                         flipped = true;
                     }
-                    if (event.action == Action.LEFT_CLICK_BLOCK) {
+                    if (event.getHand() == EnumHand.OFF_HAND) {
                         entityDaki.dropAsItem();
                         entityDaki.setDead();
                     }
                 }
             }
         }
+        
         if (flipped) {
             event.setCanceled(true);
         }
-        */
     }
 }
